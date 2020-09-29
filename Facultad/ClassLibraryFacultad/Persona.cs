67398ba4eb4d38b10385d463b0917fe1cb320766
gonzaloleg <70615605+gonzaloleg@users.noreply.github.com>
2020-09-28 22:29:03 -0300

    .
    .

--- Facultad/ClassLibraryFacultad/Persona.cs
+++ Facultad/ClassLibraryFacultad/Persona.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryFacultad
 {
-    public class Class1
+    abstract class Persona
     {
+        internal string _apellido;
+        internal string _nombre;
+        internal DateTime _fechaNac;
+
+
+        public string Apellido
+        {
+            get { return this._apellido; }
+            set { this._apellido = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+
+        // public int Edad
+        // {
+        //    get { return this._fechaNac; }
+        //    set { this._fechaNac = int.Parse(value); }
+        //}
+
+
+        public string GetNombreCompleto()
+        {
+            return Nombre + " " + Apellido;
+        }
+
+        public abstract string GetCredencial();
+
     }
 }
